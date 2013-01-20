b9b70bf3fe5e35372d9d68ec5291e8fd56c0df44
ninj0x <ninj0x@gmail.com>
2013-01-19 23:17:23 -0800

    removed requirejs nonsense, didn't need it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,27 @@
-require.config({
-  shim: {
-  },
+// require.config({
+//   shim: {
+//   },
 
-  paths: {
-    hm: 'vendor/hm',
-    esprima: 'vendor/esprima',
-    jquery: 'vendor/jquery.min'
-  }
-});
+//   paths: {
+//     hm: 'vendor/hm',
+//     esprima: 'vendor/esprima',
+//     jquery: 'vendor/jquery.min'
+//   }
+// });
 
-require(['app', 'jquery'], function(app, $) {
-  'use strict';
-  $(function() {
-    app.run();
+// require(['app', 'jquery'], function(app, $) {
+//   'use strict';
+//   $(function() {
+//     app.run();
+//   });
+// });
+
+'use strict';
+$(function() {
+  $('body').find('*').hide();
+  $('body').fadeIn(1000);
+
+  $('body').find('*').each(function(index) {
+    $(this).fadeIn(index*50);
   });
 });
